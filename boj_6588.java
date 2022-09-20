d97511a84a11eea354d378f14f8415e482f43ef7
JaeHyeonSim <112995965+JaeHyeonSim@users.noreply.github.com>
2022-09-20 15:56:54 +0900

    Update boj_6588.java

--- boj_6588.java
+++ boj_6588.java
@@ -20,19 +20,19 @@ public class Main
 	    
 	    boolean[] decimal = new boolean[max + 1];	// index에 해당하는 숫자가 소수이면 true, 아니면 false
 	    Arrays.fill(decimal, true);			// 일단 모든 수가 소수라고 가정
-	    decimal[1] = false;				// 1은 소수가 아님
+	    prime[1] = false;				// 1은 소수가 아님
 	    for (int i = 2; i <= max; i++) {
-	        if (decimal[i] == true) {
+	        if (prime[i] == true) {
 			for (int j = 2; (i * j) <= max; j++) {
-			    decimal[i * j] = false;	// i의 배수(i * j)는 소수가 아니므로 false
+			    prime[i * j] = false;	// i의 배수(i * j)는 소수가 아니므로 false
 			}
 	        }
 	    }
 	    
 	    for (int n = 0; n < cases.size() - 1; n++) {
 		    for (int i = 3; i < max; i++) {		// 3부터 max까지 홀수인 소수
-			if (decimal[i] == true) {
-			    if (decimal[cases.get(n) - i] == true) {		// testcase - 홀수인 소수 = 소수일 때, 어차피 2의 배수는 제외되므로 둘 다 홀수 소수
+			if (prime[i] == true) {
+			    if (prime[cases.get(n) - i] == true) {		// testcase - 홀수인 소수 = 소수일 때, 어차피 2의 배수는 제외되므로 둘 다 홀수 소수
 				System.out.println(cases.get(n) + " = " + i + " + " + (cases.get(n) - i));		// 즉 testcase = 홀수 소수 + 홀수 소수
 				break;
 			    }
