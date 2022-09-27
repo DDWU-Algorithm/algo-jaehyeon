3bc9f30358322626d5b11925d3773d8026c83713
JaeHyeonSim <112995965+JaeHyeonSim@users.noreply.github.com>
2022-09-27 15:13:29 +0900

    Update boj_2309.java

--- week04/boj_2309.java
+++ week04/boj_2309.java
@@ -13,7 +13,7 @@ public class Main
 		    heights.add(sc.nextInt());
 		    sum += heights.get(i);
 		}
-		sum -= 100;
+		sum -= 100;				// 백설공주의 난쟁이가 아닌 키의 합을 찾음
 		
         Collections.sort(heights);
 		for (int i = 0; i < 9; i++) {
@@ -24,7 +24,7 @@ public class Main
 		            heights.remove(i);
 		            heights.remove(j - 1);
 		            break;
-		        } else if (a + b > sum) {
+		        } else if (a + b > sum) {	// 정렬했으므로 키의 합이 sum을 넘어가면 다음 j는 보지 않음
 		            break;
 		        }
 		    }
