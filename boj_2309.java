f8a3d47f76d1447a4b50e613ba912b14395afc83
JaeHyeonSim <112995965+JaeHyeonSim@users.noreply.github.com>
2022-09-27 13:58:36 +0900

    Create boj_2309.java

---
+++ boj_2309.java
@@ -0,0 +1,40 @@
+import java.util.*;
+
+public class Main
+{
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int sum = 0;
+		ArrayList<Integer> heights = new ArrayList<Integer>();
+		for (int i = 0; i < 9; i++) {
+		    heights.add(sc.nextInt());
+		    sum += heights.get(i);
+		}
+		sum -= 100;
+		
+        Collections.sort(heights);
+		for (int i = 0; i < 9; i++) {
+		    for (int j = 1; j < 9; j++) {
+		        int a = heights.get(i);
+		        int b = heights.get(j);
+		        if (a + b == sum) {
+		            heights.remove(i);
+		            heights.remove(j - 1);
+		            break;
+		        } else if (a + b > sum) {
+		            break;
+		        }
+		    }
+		    if (heights.size() == 7) {
+		        break;
+		    }
+		}
+		
+		for (int i = 0; i < 7; i++) {
+		    System.out.println(heights.get(i));
+		}
+		
+		sc.close();
+	}
+}
